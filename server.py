bcf2cee2942a1467e0c37a04355ed46da47b6820
Arad Firouzkouhi <aradfir@gmail.com>
2022-07-25 12:30:03 +0430

    using threadpool now

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import socket
 import threading
 import importlib
+import concurrent.futures
+
 HOST = "127.0.0.1"
-PORT = 1037
+PORT = 1041
+MAX_WORKERS = 100
 client_datas = dict()
 
 
@@ -10,17 +13,16 @@ def data_calc(arg_str: str):
     # remove last item (fin)
     # cast all items to float
     # return sum
-    splited=arg_str.split()
+    splited = arg_str.split()
     try:
-        module=importlib.import_module(splited[0])
+        module = importlib.import_module(splited[0])
         func = getattr(module, splited[1])
+        print(splited)
         return func(map(float, splited[2:-1]))
     except Exception as e:
         print("No module found!")
+        raise e
     return "Error in finding module/function"
-
-
-
 
 
 def handler(conn, addr):
@@ -33,6 +35,7 @@ def handler(conn, addr):
             if not data:
                 break
             client_datas[addr] += data.decode('utf-8')
+            print(client_datas[addr])
             if client_datas[addr].endswith('fin'):
                 answer = data_calc(client_datas[addr])
                 conn.sendall(str(answer).encode())
@@ -41,9 +44,10 @@ def handler(conn, addr):
         print(f"Client {addr} disconnected!")
 
 
+pool = concurrent.futures.ThreadPoolExecutor(MAX_WORKERS)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     while True:
         s.listen()
         conn, addr = s.accept()
-        threading.Thread(target=handler, args=(conn, addr)).start()
+        t=pool.submit(handler, conn, addr)
