cd8ba25df5e0c9990f8a297b0944ab04e3637445
Arad Firouzkouhi <aradfir@gmail.com>
2022-07-25 12:11:53 +0430

    relative path for http directory lister

--- HTTP_Server.py
+++ HTTP_Server.py
@@ -1,5 +1,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse
+from os import listdir
+
+from os.path import isfile, join, isdir, dirname
+
+dirname = dirname(__file__)
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -12,7 +17,15 @@ class MyHandler(BaseHTTPRequestHandler):
         parsed = urlparse(self.path)
         query_string = parsed.query
         path = parsed.path
-        self.wfile.write(b'It works!')
+        true_path = join(dirname, path[1:])
+        print(path[1:])
+        self.wfile.write(true_path.encode())
+        onlyfiles = [f for f in listdir(true_path) if isfile(join(true_path, f))]
+        onlydirs = [f for f in listdir(true_path) if isdir(join(true_path, f))]
+        result=b"RESULT FOR : "+true_path.encode()
+        result += b"\nFILES:\n" + "\n".join(onlyfiles).encode() + b"\nDIRS:\n" + "\n".join(onlydirs).encode()
+        self.wfile.write(result)
+
 
 httpd = HTTPServer(('localhost', 10000), MyHandler)
 httpd.serve_forever()
