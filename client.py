87b32d3e4b9d8e27de2a89f09d4b461c6771a70e
Arad Firouzkouhi <aradfir@gmail.com>
2022-07-25 11:53:07 +0430

    added tests for server

--- client.py
+++ client.py
@@ -1,16 +1,21 @@
 import socket
 from random import randint
+from time import sleep
+
 HOST = "127.0.0.1"
-PORT = 1036
+PORT = 1037
 
-
-len=randint(1,20)
-l=[]
+len = randint(1, 20)
+l = []
 for i in range(len):
-    l.append(str(randint(-10000,10000)))
-str_data=" ".join(l).encode() + b" fin"
+    l.append(str(randint(-10000, 10000)))
+str_data = b"builtins sum " + " ".join(l).encode() + b" fin"
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
-    s.sendall(str_data)
-    data=s.recv(1024)
+    s.connect((HOST, PORT))
+    print("sending part 1")
+    s.sendall(str_data[:10])
+    sleep(5)
+    print("sending part 2")
+    s.sendall(str_data[10:])
+    data = s.recv(1024)
     print(f"Sum of {l} is {data.decode()}")
